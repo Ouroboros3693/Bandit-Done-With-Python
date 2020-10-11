e42e680cfdbd77d285b30dcd992c2a3353cec9ea
Ouroboros3693 <64241412+Ouroboros3693@users.noreply.github.com>
2020-10-11 19:59:05 +1100

    Create bandit7.py
    Returns key to bandit8

---
+++ bandit7.py
@@ -0,0 +1,17 @@
+import paramiko
+
+hostname = "bandit.labs.overthewire.org"
+port = 2220
+username = "bandit7"
+password = "HKBPTKQnIay4Fw76bEy8PVxKEDQRKTzs"
+
+cmd = "cat data.txt | grep millionth | cut -d 'h' -f2"
+#connect to server
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname, port, username, password)
+#connect to server
+#read the readme in server and print
+stdin, stdout, stderr = ssh.exec_command(cmd)
+lines = stdout.readlines()
+print(lines)
